a501356cf97fe624d8c7bc21f671c2d03e0465a1
Igor Elkanyuk <ielkanyuk@MacBook-Pro-Igor.local>
2024-02-22 14:41:07 +0500

    Add button events

---
+++ src/components/Categories.jsx
@@ -0,0 +1,36 @@
+import { useState } from "react";
+
+function Categories() {
+  const [activeIndex, setActiveIndex] = useState(0);
+
+  const categories = [
+    "Все",
+    "Мясные",
+    "Вегетарианская",
+    "Гриль",
+    "Острые",
+    "Закрытые",
+  ];
+
+  const onClickCategory = (index) => {
+    setActiveIndex(index);
+  };
+
+  return (
+    <div className="categories">
+      <ul>
+        {categories.map((name, index) => (
+          <li
+            key={index}
+            onClick={() => onClickCategory(index)}
+            className={activeIndex === index ? "active" : ""}
+          >
+            {name}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default Categories;
